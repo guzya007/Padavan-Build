fa80f30fbf9a07ad4ac0509810f4f729fc0c24e2
guzya007 <guzya007@yandex.ru>
2025-04-28 16:17:57 +0300

    Update setwifi.sh

--- custom/scripts/setwifi.sh
+++ custom/scripts/setwifi.sh
@@ -6,14 +6,14 @@ wifi_5g_pwd=$4
 defaultfile=$5
 
 if [ -n "$wifi_2g_ssid" ]; then
-	sed -i "40s/BOARD_PID \"_%s\"/\"$wifi_2g_ssid\"/g" $defaultfile
+	sed -i "40s/\"wrt\"/\"$wifi_2g_ssid\"/g" $defaultfile
 fi
 if [ -n "$wifi_2g_pwd" ]; then
-	sed -i "44s/\"1234567890\"/\"$wifi_2g_pwd\"/g" $defaultfile
+	sed -i "44s/\"vadya007\"/\"$wifi_2g_pwd\"/g" $defaultfile
 fi
 if [ -n "$wifi_5g_ssid" ]; then
-	sed -i "41s/BOARD_PID \"_5G_%s\"/\"$wifi_5g_ssid\"/g" $defaultfile
+	sed -i "41s/\"wrt5\"/\"$wifi_5g_ssid\"/g" $defaultfile
 fi
 if [ -n "$wifi_5g_pwd" ]; then
-        sed -i "45s/\"1234567890\"/\"$wifi_5g_pwd\"/g" $defaultfile
+        sed -i "45s/\"vadya007\"/\"$wifi_5g_pwd\"/g" $defaultfile
 fi
